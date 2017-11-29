4d1acde70c2674ba01e322f0d8f082d5e7295ba5
James Frost <james.frost@automattic.com>
2017-11-29 16:41:11 +0000

    Media Library: Disable selection of non-synced items.

--- WordPress/Classes/ViewRelated/Media/MediaLibraryViewController.swift
+++ WordPress/Classes/ViewRelated/Media/MediaLibraryViewController.swift
@@ -857,13 +857,26 @@ extension MediaLibraryViewController: WPMediaPickerViewControllerDelegate {
     func mediaPickerController(_ picker: WPMediaPickerViewController, previewViewControllerFor asset: WPMediaAsset) -> UIViewController? {
         guard picker == pickerViewController else { return WPAssetViewController(asset: asset) }
 
+        guard let media = asset as? Media,
+            media.remoteStatus == .sync else {
+                return nil
+        }
+
         WPAppAnalytics.track(.mediaLibraryPreviewedItem, with: blog)
         return mediaItemViewController(for: asset)
     }
 
     func mediaPickerController(_ picker: WPMediaPickerViewController, shouldSelect asset: WPMediaAsset) -> Bool {
         guard picker == pickerViewController else { return true }
-        guard !isEditing else { return true }
+
+        guard let media = asset as? Media,
+            media.remoteStatus == .sync else {
+                return false
+        }
+
+        guard !isEditing else {
+            return true
+        }
 
         if let viewController = mediaItemViewController(for: asset) {
             WPAppAnalytics.track(.mediaLibraryPreviewedItem, with: blog)
